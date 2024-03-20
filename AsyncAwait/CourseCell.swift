5fad11b2b167fad245a49f01aa52e7e4a8162d78
Vlad-tdk <xalif80@gmail.com>
2024-03-20 14:39:01 +0100

    add cell

--- AsyncAwait/CourseCell.swift
+++ AsyncAwait/CourseCell.swift
@@ -5,4 +5,28 @@
 //  Created by Vlad on 20.3.24..
 //
 
-import Foundation
+import UIKit
+
+final class CourseCell: UITableViewCell {
+
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessons: UILabel!
+    @IBOutlet var numberOfTests: UILabel!
+    
+    func configure(with course: Course) {
+        courseNameLabel.text = course.name
+        numberOfLessons.text = "Number of lessons: \(course.numberOfLessons)"
+        numberOfTests.text = "Number of tests: \(course.numberOfTests)"
+        
+        guard let url = URL(string: course.imageUrl) else { return }
+        
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            
+            DispatchQueue.main.async {
+                self.courseImage.image = UIImage(data: imageData)
+            }
+        }
+    }
+}
